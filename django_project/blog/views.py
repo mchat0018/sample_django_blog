8a234921a17f379daed8a5735fffa0a20404fd5c
Mihir Chaturvedi <mihir19175@iiitd.ac.in>
2021-12-24 17:28:20 +0530

    Added templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author':'MihirC',
+        'title': 'Blog Post 1',
+        'content':'First Post Content',
+        'date_posted': 'December 24, 2021'
+    },
+    {
+        'author':'Jane Doe',
+        'title': 'Blog Post 2',
+        'content':'Second Post Content',
+        'date_posted': 'December 25, 2021'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
